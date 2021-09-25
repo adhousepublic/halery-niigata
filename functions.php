927107337fac0623844a953b23a2993e3787cb25
steeq-maruyama <k.maruyama@steeq.jp>
2021-09-25 16:22:40 +0900

    210925:ページャー設置→リダイレクト制御→修正→CSS追加→CSS調整→修正→HTMLケシ→リダイレクト処理調整→修正→修正

--- functions.php
+++ functions.php
@@ -179,14 +179,7 @@ add_filter('redirect_canonical','my_disable_redirect_canonical');
 function my_disable_redirect_canonical( $redirect_url ) {
 
   if ( is_home() || is_archive() ){
-    $subject = $redirect_url;
-    $pattern = '/\/page\//'; // URLに「/page/」があるかチェック
-    preg_match($pattern, $subject, $matches);
-
-    if ($matches){
-      //リクエストURLに「/page/」があれば、リダイレクトしない。
       $redirect_url = false;
       return $redirect_url;
-    }
   }
 }
